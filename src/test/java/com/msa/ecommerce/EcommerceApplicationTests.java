a437dffa68f1ab131309d988d0405a0f149e3338
joo <vividjtj@gmail.com>
2022-05-24 23:56:09 +0900

    eureka settings

---
+++ src/test/java/com/msa/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
